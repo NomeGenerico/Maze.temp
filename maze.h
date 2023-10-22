d11a7e95c3f39e24159dbce09ab37694b00d7227
Ricardo Torres <70348286+NomeGenerico@users.noreply.github.com>
2023-10-22 00:32:36 -0300

    Create maze.h

---
+++ maze.h
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#pragma once
+
+using namespace std;
+
+
+class cell
+{
+public:
+
+bool WallNorth;
+bool WallEast;
+bool WallSouth;
+bool WallWest;
+bool Visited;
+
+
+
+cell ()
+	{
+		bool WallNorth = true;
+		bool WallEast  = true;
+		bool WallSouth = true;
+		bool WallWest  = true;
+		bool Visited   = true;
+		cout << "test";
+	};
+
+
+};
+	
