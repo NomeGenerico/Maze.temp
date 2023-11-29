4aa2fa9df1e60d650059350df482e9fdf7c6566e
Ricardo Torres <70348286+NomeGenerico@users.noreply.github.com>
2023-11-28 21:41:41 -0300

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -1,20 +1,26 @@
-#include "Maze.h" 
-#include <iostream> 
-  
- using namespace std; 
- int main() 
- { 
-         int SizeX, SizeY; 
-         cout << "Size X:"; 
-         cin >> SizeX; 
-         cout << "Size y:"; 
-         cin >> SizeY; 
-  
-         vector<vector<cell>> MazeArray(SizeY, vector<cell>(SizeX));         
-         cout << MazeArray[0][0].WallSouth << endl;  
-         
-         
-       
-   
-   
- }
+#include "Maze.h"
+#include <iostream>
+
+
+
+
+
+
+int main()
+
+
+	int SizeX, SizeY;
+	std::cout << "Size X:";
+	std::cin >> SizeX;
+	std::cout << "Size y:";
+	std::cin >> SizeY;
+
+	MazeArray = std::vector<std::vector<cell>>(SizeY, std::vector<cell>(SizeX, cell()));
+	std::cout << MazeArray.capacity() << std::endl;
+	std::cout << MazeArray[1][1].WallSouth << std::endl;
+	
+
+
+
+
+}
