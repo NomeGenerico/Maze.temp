a6b0222eaaac68bdbb06e04bfd3430e2c93f58f7
Ricardo Torres <70348286+NomeGenerico@users.noreply.github.com>
2024-01-06 13:18:45 -0300

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -1,22 +1,28 @@
 #include "Maze.h"
+#include "BackTracker.h"
 #include <iostream>
 #include "SFML\Graphics.hpp"
 #include "SFML\Window.hpp"
 #include "SFML\System.hpp"
+#include <time.h>
 
 // declares variables
 int SizeX, SizeY;
 sf::RenderWindow window;
 
-// initializes some variables
-sf::Vector2f WallSize = sf::Vector2f(10, 2);
-sf::Vector2f CellSize = sf::Vector2f(10, 10);
+int WindowSizeX;
+int WindowSizeY;
 
-sf::RectangleShape Cell = sf::RectangleShape(CellSize);
-sf::RectangleShape Wall = sf::RectangleShape(WallSize);
-sf::RectangleShape RotatedWall = sf::RectangleShape(WallSize);
+sf::Vector2f WallSize;
+sf::Vector2f CellSize;
+
+sf::RectangleShape Cell;
+sf::RectangleShape Wall;
+sf::RectangleShape WallR;
 
 
+
+// Doesnt Work IDKW
 int renderCell(int x, int y, std::vector<std::vector<cell>> MazeArray)
 {
     int PixelCordX = x * 10;
@@ -29,38 +35,74 @@ int renderCell(int x, int y, std::vector<std::vector<cell>> MazeArray)
     {
         Cell.setFillColor(sf::Color::Green);
     }
-    RotatedWall.rotate(90.f);
     Cell.setPosition(PixelCordX, PixelCordY);
     Wall.setPosition(PixelCordX, PixelCordY);
     window.draw(Cell);
     window.draw(Wall);
-    window.draw(RotatedWall);
     Wall.move(8, 0);
-    RotatedWall.move(0, 8);
     window.draw(Wall);
-    window.draw(RotatedWall);
     return 0;
 }
+// Doesnt Work IDKW
 int renderGrid(std::vector<std::vector<cell>> MazeArray)
 {
     for (int x = 0; x < MazeArray.size(); x++)
     {
         for (int y = 0; y < MazeArray[0].size(); y++)
         {
-            renderCell(x,y,MazeArray);
+            renderCell(x, y, MazeArray);
         }
     }
     return 0;
 }
 
+int mazesolver(int x, int y)
+{
+    return 0;
+}
+
+
+
+
+class Button
+{
+    sf::Vector2f ButtonSize = sf::Vector2f(100, 30);
+    sf::RectangleShape ButtonImage = sf::RectangleShape(ButtonSize);
+    int Id;
+    coordinate Position;
+    
+    Button()
+    {
+
+        ButtonImage.setOutlineColor(sf::Color::Black);
+        ButtonImage.setOutlineThickness(-2);
+    }
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 int main()
 {
-    int WindowSizeX = 500;
-    int WindowSizeY = 500;
-    sf::RenderWindow window(sf::VideoMode(1000, 1000), "fuck you!", sf::Style::Default);
-    window.setFramerateLimit(165);
+    srand(time(nullptr));
+    int WindowSizeX = 1200;
+    int WindowSizeY = 800;
+    sf::RenderWindow window(sf::VideoMode(WindowSizeX, WindowSizeY), "fuck jordam?", sf::Style::Default);
+    window.setFramerateLimit(60);
 
     int SizeX, SizeY;
     std::cout << "Size X:";
@@ -68,14 +110,41 @@ int main()
     std::cout << "Size y:";
     std::cin >> SizeY;
 
-    std::vector<std::vector<cell>> MazeArray = std::vector<std::vector<cell>>(SizeY, std::vector<cell>(SizeX, cell()));
+    // Seting Up Maze
+    std::vector<std::vector<cell>> MazeArray = std::vector<std::vector<cell>>(SizeX, std::vector<cell>(SizeY, cell()));
     std::cout << MazeArray.capacity() << std::endl;
     std::cout << MazeArray[1][1].WallSouth << std::endl;
-    std::cout << MazeArray[1][1].Visited;
+
+
+    //Size of the maze on screen
+    float CellSizeInt = (WindowSizeY - (50 / SizeY)) / SizeY;
+    sf::Vector2f WallSize = sf::Vector2f(CellSizeInt, 1);
+    sf::Vector2f CellSize = sf::Vector2f(CellSizeInt, CellSizeInt);
+
+    sf::RectangleShape Cell = sf::RectangleShape(CellSize);
+    sf::RectangleShape Wall = sf::RectangleShape(WallSize);
+    sf::RectangleShape WallR = sf::RectangleShape(WallSize);
+    WallR.setRotation(90.f);
+    coordinate startCord = coordinate(0, 0);
+    bool MazeGenerated = false;
+    bool Backtracker = true;
 
 
 
 
+    std::stack <coordinate, std::deque<coordinate>> stack;
+    int numVisitedCells = 1;
+    bool ended = false;
+    coordinate tempCord = startCord;
+    MazeArray[startCord.y][startCord.x].Visited = true;
+
+    stack.push(tempCord);
+
+
+
+
+
+    int fuckme = 0;
 
 
     while (window.isOpen())
@@ -84,27 +153,140 @@ int main()
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
+            {
                 window.close();
+            }
         }
-        window.clear(sf::Color::Black);
-        renderGrid(MazeArray);
-        renderCell(1, 1,MazeArray);
-        /*
-        RotatedWall.rotate(40.f);
-        window.draw(Cell);
-        window.draw(Wall);
-        window.draw(RotatedWall);
-        Wall.move(8, 0);
-        RotatedWall.move(0, 8);
-        window.draw(Wall);
-        window.draw(RotatedWall);
-        */
+        
+
+        
+
+
+        
+        
+        if (!MazeGenerated and Backtracker)
+        {
+            if (numVisitedCells == SizeX * SizeY)
+            {
+                MazeArray = CleanMaze(MazeArray);
+                MazeGenerated = true;
+            }
+
+            // Makes cell visited, removes walls and pushes the stack
+            tempCord = AdjRandomCords(SizeX, SizeY, tempCord, MazeArray);
+            if (!(tempCord.x == -1) and !(tempCord.y == -1))
+            {
+                MazeArray[tempCord.x][tempCord.y].Visited = true;
+                numVisitedCells += 1;
+                MazeArray = wallremover(SizeX, SizeY, stack.top(), tempCord, MazeArray);
+                stack.push(tempCord);
+            }
+            else
+            {
+                bool valid = false;
+
+                while (!valid)
+                {
+
+                    if (stack.size() == 0)
+                    {
+                        MazeArray = CleanMaze(MazeArray);
+                        MazeGenerated = true;
+                        break;
+                    }
+                    stack.pop(); 
+                    if (stack.size() == 0)
+                    {
+                        MazeArray = CleanMaze(MazeArray);
+                        MazeGenerated = true;
+                        break;
+                    }
+                    tempCord = stack.top();
+                    valid = CheckNeighbors(tempCord, SizeX, SizeY, MazeArray);
+                    
+                    
+                }
+            }
+
+
+            
+        }
+        
+        if (MazeGenerated and fuckme < 1)
+        {
+            fuckme = 1;
+            MazeArray = CleanMaze(MazeArray);
+        }
+        //MazeArray = CleanMaze(MazeArray);
 
 
 
 
-        window.display();
-        //std::cout << ("pica");
+
+
+
+
+
+            // works ouside the funtion
+            // draws the maze
+            // Sometimes Walls or The backtraking funtion Dont work as expected????
+      window.clear(sf::Color::White);
+      for (int x = 0; x < SizeX; x++) {
+        for (int y = 0; y < SizeY; y++) {
+                    int PixelCordX = x * CellSize.x + WallSize.y;
+                    int PixelCordY = y * CellSize.y + WallSize.y;
+
+                    if (MazeArray[x][y].Visited and !(x == 0 and y == 0) and !(x == SizeX - 1 and y == SizeY - 1))
+                    {
+                        Cell.setFillColor(sf::Color::Blue);
+                    }
+                    if (!(MazeArray[x][y].Visited) and !(x == 0 and y == 0) and !(x == SizeX - 1 and y == SizeY - 1))
+                    {
+                        Cell.setFillColor(sf::Color::Black);
+                    }
+
+                    if (x == 0 and y == 0)
+                    {
+                        Cell.setFillColor(sf::Color::Cyan);
+                    }
+                    if (x == SizeX - 1 and y == SizeY - 1)
+                    {
+                        Cell.setFillColor(sf::Color::Red);
+                    }
+
+
+
+
+                    Cell.setPosition(PixelCordX, PixelCordY);
+                    Wall.setPosition(PixelCordX, PixelCordY);
+                    WallR.setPosition(PixelCordX + 1, PixelCordY);
+                    window.draw(Cell);
+                    if (MazeArray[x][y].WallNorth)
+                    {
+                        window.draw(Wall);
+                    }
+                    if (MazeArray[x][y].WallWest)
+                    {
+                        window.draw(WallR);
+                    }
+                    Wall.move(0, CellSize.x - WallSize.y);
+                    WallR.move(CellSize.y - 1, 0);
+                    if (MazeArray[x][y].WallSouth)
+                    {
+                        window.draw(Wall);
+                    }
+                    if (MazeArray[x][y].WallEast)
+                    {
+                        window.draw(WallR);
+                    }
+        }
+      }
+
+
+
+            window.display();
+            //std::cout << ("pica");
+
     }
     return 0;
 }
